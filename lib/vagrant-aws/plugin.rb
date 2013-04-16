6bea4fc92fb26df834f9fc19f60bac5cdd290dde
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-04-16 14:56:56 -0700

    Update requirement for Vagrant to be 1.2.0

--- lib/vagrant-aws/plugin.rb
+++ lib/vagrant-aws/plugin.rb
@@ -6,8 +6,8 @@ end
 
 # This is a sanity check to make sure no one is attempting to install
 # this into an early Vagrant version.
-if Vagrant::VERSION < "1.1.0"
-  raise "The Vagrant AWS plugin is only compatible with Vagrant 1.1+"
+if Vagrant::VERSION < "1.2.0"
+  raise "The Vagrant AWS plugin is only compatible with Vagrant 1.2+"
 end
 
 module VagrantPlugins
