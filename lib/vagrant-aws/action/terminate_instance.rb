972696b937c1dbc672b002c23786ec9e4d514a5f
Ben Whaley <bwhaley@apigee.com>
2013-05-27 23:04:49 -0700

    release elastic ip address upon termination

--- lib/vagrant-aws/action/terminate_instance.rb
+++ lib/vagrant-aws/action/terminate_instance.rb
@@ -1,4 +1,5 @@
 require "log4r"
+require "json"
 
 module VagrantPlugins
   module AWS
@@ -23,8 +24,28 @@ module VagrantPlugins
             env[:ui].info(I18n.t("vagrant_aws.elastic_ip_deallocated"))
           end
           env[:machine].id = nil
+  
+          # Release the elastic IP
+          ip_file = env[:machine].data_dir.join('elastic_ip')
+          if ip_file.file?
+            release_address(env,ip_file.read)
+            ip_file.delete 
+          end
 
           @app.call(env)
+        end
+
+        # Release an elastic IP address
+        def release_address(env,eip)
+          h = JSON.parse(eip)
+          # Use association_id and allocation_id for VPC, use public IP for EC2
+          if h['association_id']
+            env[:aws_compute].disassociate_address(nil,h['association_id'])
+            env[:aws_compute].release_address(h['allocation_id'])
+          else
+            env[:aws_compute].disassociate_address(h['public_ip'])
+            env[:aws_compute].release_address(h['public_ip'])
+          end
         end
       end
     end
