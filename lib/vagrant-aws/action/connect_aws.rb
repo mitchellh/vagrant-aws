c4fd9814c0895d405efbe550ccf703e75a751df1
David Rogers AKA "AL the X <al-the-x@users.noreply.github.com>
2020-08-20 15:08:05 -0400

    Use long-hand `Hash#slice` + `Hash#keys`
    Since `Hash#except` is not in standard Ruby and Vagrant removed `active_support` that provides the polyfill.
    
    Resolves mitchellh/vagrant-aws#566

--- lib/vagrant-aws/action/connect_aws.rb
+++ lib/vagrant-aws/action/connect_aws.rb
@@ -38,7 +38,7 @@ module VagrantPlugins
 
           @logger.info("Connecting to AWS...")
           env[:aws_compute] = Fog::Compute.new(fog_config)
-          env[:aws_elb]     = Fog::AWS::ELB.new(fog_config.except(:provider, :endpoint))
+          env[:aws_elb]     = Fog::AWS::ELB.new(fog_config.slice(fog_config.keys - [:provider, :endpoint]))
 
           @app.call(env)
         end
